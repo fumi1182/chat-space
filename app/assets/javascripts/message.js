baee7badb1a1b432abde3fb2384a761de972843f
fumi1182 <takafumi@ootsukatakashinoMacBook-Pro.local>
2020-02-28 20:05:29 +0900

    aaaa

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){ 
   var buildHTML = function(message) {
+
     if (message.content && message.image) {
      var html =
       `<div class="message" data-message-id=${message.id}>
@@ -18,6 +19,24 @@ $(function(){
          </div>
          <img src=${message.image} >
        </div>`
+      } else if (message.content) {
+        //同様に、data-idが反映されるようにしている
+        var html = `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              message.user_name 
+            </div>
+            <div class="upper-message__date">
+              message.created_at 
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              message.content
+            </p>
+          </div>
+        </div>`
+
      return html;
    } else if (message.image) {
      var html =
@@ -40,10 +59,35 @@ $(function(){
      return html;
    };
  }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
+
+ $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+   .done(function(data){
+     var html = buildHTML(data);
+     $('.chat-main__message-list').append(html);      
+     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+     $('form')[0].reset();
+   })
+   .fail(function(data) {
+       alert("メッセージ送信に失敗しました。")
+   })
+   .always(function(data){
+     $('.form__submit').prop('disabled', false);　
+   })
+ })
+
+
+ var reloadMessages = function() {
  var last_message_id = $('.message:last').data("message-id");
  $.ajax({
   //ルーティングで設定した通りのURLを指定
@@ -74,7 +118,7 @@ $('#new_message').on('submit', function(e){
   .always(function(data){
     $('.form__submit').prop('disabled', false);　
   });
-});
+};
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
   setInterval(reloadMessages, 7000);
 }
