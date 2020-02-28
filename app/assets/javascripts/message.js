c4d2a3f5cb021fa278f7baa88b6d2f51c4899360
fumi1182 <takafumi@ootsukatakashinoMacBook-Pro.local>
2020-02-28 21:56:02 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,6 +62,38 @@ $(function(){
     };
   };
 
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    
+    
+    
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log(html)
+      $('.chat-main__message-list').append(html);      
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      
+    })
+    .fail(function(data) {
+        alert("メッセージ送信に失敗しました。")
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);　
+    })
+  })
+
+
   var reloadMessages = function() {
   var last_message_id = $('.message:last').data("message-id");
   $.ajax({
