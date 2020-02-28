e0ee13a550e82e00f2bc20365a4ed8950fd6af59
fumi1182 <takafumi@ootsukatakashinoMacBook-Pro.local>
2020-02-28 21:30:54 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,7 +9,7 @@ $(function(){
              ${message.user_name}
            </div>
            <div class="upper-message__date">
-             ${message.date}
+             ${message.created_at}
            </div>
          </div>
          <div class="lower-message">
@@ -19,16 +19,16 @@ $(function(){
          </div>
          <img src=${message.image} >
        </div>`
+       return html;
       } else if (message.content) {
         //同様に、data-idが反映されるようにしている
-        var html = 
-        `<div class="message" data-message-id=${message.id}>
+        var html = `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name} 
             </div>
             <div class="upper-message__date">
-              ${message.created_at} 
+              ${message.created_at}
             </div>
           </div>
           <div class="lower-message">
@@ -38,89 +38,63 @@ $(function(){
           </div>
         </div>`
 
-     return html;
-   } else if (message.image) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-       
-     return html;
-   };
- }
+      return html;
+    } else if (message.image) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+        
+      return html;
+    };
+  };
 
- $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+  var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
   $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
+    //ルーティングで設定した通りのURLを指定
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
     dataType: 'json',
-    processData: false,
-    contentType: false
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
   })
-   .done(function(data){
-     var html = buildHTML(data);
-     $('.chat-main__message-list').append(html);      
-     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-     $('form')[0].reset();
-   })
-   .fail(function(data) {
-       alert("メッセージ送信に失敗しました。")
-   })
-   .always(function(data){
-     $('.form__submit').prop('disabled', false);　
-   })
- })
+    .done(function(messages){
+      if (messages.length !== 0) {
 
-
- var reloadMessages = function() {
- var last_message_id = $('.message:last').data("message-id");
- $.ajax({
-  //ルーティングで設定した通りのURLを指定
-  url: "api/messages",
-  //ルーティングで設定した通りhttpメソッドをgetに指定
-  type: 'get',
-  dataType: 'json',
-  //dataオプションでリクエストに値を含める
-  data: {id: last_message_id}
- })
-  .done(function(data){
-    if (messages.length !== 0) {
-
-      //追加するHTMLの入れ物を作る
-      var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      //メッセージが入ったHTMLに、入れ物ごと追加
-      $('.chat-main__message-list').append(insertHTML);
-      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-    }
-  })
-  .fail(function(data) {
-      alert("メッセージ送信に失敗しました。");
-  })
-  .always(function(data){
-    $('.form__submit').prop('disabled', false);　
-  });
-};
-if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 7000);
-}
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function(data) {
+        alert("メッセージ送信に失敗しました。");
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);　
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
